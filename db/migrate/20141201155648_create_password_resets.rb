5ef0d5f99f5b76a13d9e14bec383300d70134f0d
bergerjosh11 <bergerjosh11@gmail.com>
2014-12-03 13:17:25 -0500

    Got rid of gem-generated comments in a couple of the config files

--- db/migrate/20141201155648_create_password_resets.rb
+++ db/migrate/20141201155648_create_password_resets.rb
@@ -2,7 +2,7 @@ class CreatePasswordResets < ActiveRecord::Migration
   def change
     create_table :password_resets do |t|
       t.belongs_to :user, index: true
-      t.string :token
+      t.string :token null: false
 
       t.timestamps null: false
     end

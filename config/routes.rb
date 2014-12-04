a942cdf59eabd5f834a26be22cc731ad0ce99a8a
bergerjosh11 <bergerjosh11@gmail.com>
2014-12-04 15:58:42 -0500

    refactored the events controller, edited some unnecesary lines out of the dashboard/show. updated the routes to reflect the events controller, removed two unneccesary files that were gem generated.

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
 
-  resources :events, only: [:new, :create, :destroy, :index, :show, :update, :edit]
+  resources :events, only: [:new, :create, :destroy, :show, :update, :edit]
 end

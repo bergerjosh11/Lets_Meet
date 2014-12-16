ec05809fc6ba146a547e6b8fa3dcca789bacd3f0
bergerjosh11 <bergerjosh11@gmail.com>
2014-12-16 12:23:41 -0500

    fix config files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,8 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { :host => "m" }
   config.cache_classes = false
+  config.allow_concurrency = true
+  config.eager_load = false
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
   config.action_mailer.raise_delivery_errors = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,8 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { :host => "n" }
   config.cache_classes = true
   config.eager_load = true
-  config.consider_all_requests_local = false
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.serve_static_assets = false
   config.assets.js_compressor = :uglifier

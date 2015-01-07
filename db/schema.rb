8c8c9c845be98154b0ddce161605dbed754bf8c4
bergerjosh11 <bergerjosh11@gmail.com>
2015-01-07 17:14:56 -0500

    Added bootcamp styling gem to convert linear dashboard UL to a grid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203202708) do
+ActiveRecord::Schema.define(version: 20141208153305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,15 +27,6 @@ ActiveRecord::Schema.define(version: 20141203202708) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  create_table "password_resets", force: true do |t|
-    t.integer  "user_id"
-    t.string   "token"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "password_resets", ["user_id"], name: "index_password_resets_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",           null: false
